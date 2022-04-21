14312e2fff50b66af2ebb7d1c0dcf8af1ffc39e0
7x8y <79317062+7x8y@users.noreply.github.com>
2022-04-21 16:18:33 +0530

    Add files via upload

---
+++ client/build/precache-manifest.f5d2212d75c56ed7e2abb21d80332ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b1346eefec49213a130cf5d7bf724c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27508bf1a1923060caf2",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "992bfd0353461c9e3b69",
+    "url": "/static/js/2.42cc6df8.chunk.js"
+  },
+  {
+    "revision": "1331a50430c4cf87c9eb10eead2b1fbf",
+    "url": "/static/js/2.42cc6df8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27508bf1a1923060caf2",
+    "url": "/static/js/main.1ae8e4c6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
